b46cc2859ee00212446e313b8592ba779b773f84
hsufit <a167349852@hotmail.com>
2021-09-23 01:47:04 +0800

    Move transaction implementation to bus

--- bus.cpp
+++ bus.cpp
@@ -8,10 +8,30 @@ BUS::BUS(sc_module_name name): sc_module(name), memory("memory")
 
 int32_t BUS::read(uint32_t addr, uint32_t size)
 {
-	return 0;
+	char data;
+	tlm::tlm_generic_payload trans;
+	trans.set_command(tlm::TLM_READ_COMMAND);
+	trans.set_data_ptr(reinterpret_cast<unsigned char*>(&data));
+	trans.set_data_length(size);
+	trans.set_response_status(tlm::TLM_INCOMPLETE_RESPONSE);
+	trans.set_address(addr);
+	sc_core::sc_time delay = sc_core::SC_ZERO_TIME;
+	memory_socket->b_transport(trans, delay);
+	return data;
 }
 
-void BUS::write(uint32_t addr, uint32_t data, uint32_t size) {}
+void BUS::write(uint32_t addr, uint32_t data, uint32_t size)
+
+{
+	tlm::tlm_generic_payload trans;
+	trans.set_command(tlm::TLM_WRITE_COMMAND);
+	trans.set_data_ptr(reinterpret_cast<unsigned char*>(&data));
+	trans.set_data_length(size);
+	trans.set_response_status(tlm::TLM_INCOMPLETE_RESPONSE);
+	trans.set_address(addr);
+	sc_core::sc_time delay = sc_core::SC_ZERO_TIME;
+	memory_socket->b_transport(trans, delay);
+}
 
 
 
--- cpu.cpp
+++ cpu.cpp
@@ -16,17 +16,7 @@ void CPU::hello_thread(void)
 
 void CPU::step()
 {
-	char data;
-	sc_dt::uint64 addr = pc;
-	int size = 1;
-	tlm::tlm_generic_payload trans;
-	trans.set_command(tlm::TLM_READ_COMMAND);
-	trans.set_data_ptr(reinterpret_cast<unsigned char*>(&data));
-	trans.set_data_length(size);
-	trans.set_response_status(tlm::TLM_INCOMPLETE_RESPONSE);
-	trans.set_address(addr);
-	sc_core::sc_time delay = sc_core::SC_ZERO_TIME;
-	memory_socket->b_transport(trans, delay);
+	char data = address_space->read(pc, 1);
 	std::cout << "time " << sc_core::sc_time_stamp() << ":" << "hello received!!" << data << std::endl;
 	pc++;
 }
