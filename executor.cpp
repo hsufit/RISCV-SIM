8534f3f1037a6e71a3ab5bbceeecb9e435576b31
hsufit <a167349852@hotmail.com>
2021-09-25 23:31:06 +0800

    Fold duplicated if statement into switch

--- executor.cpp
+++ executor.cpp
@@ -4,43 +4,65 @@
 
 void EXECUTOR::execute()
 {
-	if(instruction_decoder->get_opcode() == INSTRUCTION_DECODER_INTERFACE::IMM_OP &&
-	   instruction_decoder->get_func3() == INSTRUCTION_DECODER_INTERFACE::ADDI_FN3) {
-		auto rs1 = instruction_decoder->get_rs1();
-		auto rd = instruction_decoder->get_rd();
 
-		auto value = register_file->get_value_integer(rs1) + instruction_decoder->get_imm(31, 20);
-		register_file->set_value_integer(rd, value);
-		//TODO: just for test, move these to decoder and logger in future
-		std::cout << "ADDI" << std::endl;
-		std::cout << "rs1: " << rs1 << std::endl;
-		std::cout << "rd: " << rd << std::endl;
-		std::cout << "value: " << value << std::endl;
+	switch (instruction_decoder->get_opcode()) {
+		case INSTRUCTION_DECODER_INTERFACE::IMM_OP:
+			switch (instruction_decoder->get_func3()) {
+				case INSTRUCTION_DECODER_INTERFACE::ADDI_FN3:
+					if(instruction_decoder->get_opcode() == INSTRUCTION_DECODER_INTERFACE::IMM_OP &&
+					   instruction_decoder->get_func3() == INSTRUCTION_DECODER_INTERFACE::ADDI_FN3) {
+						auto rs1 = instruction_decoder->get_rs1();
+						auto rd = instruction_decoder->get_rd();
+
+						auto value = register_file->get_value_integer(rs1) + instruction_decoder->get_imm(31, 20);
+						register_file->set_value_integer(rd, value);
+						//TODO: just for test, move these to decoder and logger in future
+						std::cout << "ADDI" << std::endl;
+						std::cout << "rs1: " << rs1 << std::endl;
+						std::cout << "rd: " << rd << std::endl;
+						std::cout << "value: " << value << std::endl;
+					}
+					break;
+				case INSTRUCTION_DECODER_INTERFACE::ANDI_FN3:
+					if(instruction_decoder->get_opcode() == INSTRUCTION_DECODER_INTERFACE::IMM_OP &&
+					   instruction_decoder->get_func3() == INSTRUCTION_DECODER_INTERFACE::ANDI_FN3) {
+						auto rs1 = instruction_decoder->get_rs1();
+						auto rd = instruction_decoder->get_rd();
+
+						auto value = register_file->get_value_integer(rs1) & instruction_decoder->get_imm(31, 20);
+						register_file->set_value_integer(rd, value);
+					}
+					break;
+				case INSTRUCTION_DECODER_INTERFACE::XORI_FN3:
+					if(instruction_decoder->get_opcode() == INSTRUCTION_DECODER_INTERFACE::IMM_OP &&
+					   instruction_decoder->get_func3() == INSTRUCTION_DECODER_INTERFACE::ORI_FN3) {
+						auto rs1 = instruction_decoder->get_rs1();
+						auto rd = instruction_decoder->get_rd();
+
+						auto value = register_file->get_value_integer(rs1) | instruction_decoder->get_imm(31, 20);
+						register_file->set_value_integer(rd, value);
+					}
+					break;
+				case INSTRUCTION_DECODER_INTERFACE::ORI_FN3:
+					if(instruction_decoder->get_opcode() == INSTRUCTION_DECODER_INTERFACE::IMM_OP &&
+					   instruction_decoder->get_func3() == INSTRUCTION_DECODER_INTERFACE::XORI_FN3) {
+						auto rs1 = instruction_decoder->get_rs1();
+						auto rd = instruction_decoder->get_rd();
+
+						auto value = register_file->get_value_integer(rs1) ^ instruction_decoder->get_imm(31, 20);
+						register_file->set_value_integer(rd, value);
+					}
+					break;
+				default:
+					std::cout << "INVALID: Func3 in IMM_OP :" << instruction_decoder->get_func3() << std::endl;
+					break;
+			}
+			break;
+		default:
+			std::cout << "INVALID: Func3 in IMM_OP :" << instruction_decoder->get_func3() << std::endl;
+			break;
 	}
-	if(instruction_decoder->get_opcode() == INSTRUCTION_DECODER_INTERFACE::IMM_OP &&
-	   instruction_decoder->get_func3() == INSTRUCTION_DECODER_INTERFACE::ANDI_FN3) {
-		auto rs1 = instruction_decoder->get_rs1();
-		auto rd = instruction_decoder->get_rd();
 
-		auto value = register_file->get_value_integer(rs1) & instruction_decoder->get_imm(31, 20);
-		register_file->set_value_integer(rd, value);
-	}
 
-	if(instruction_decoder->get_opcode() == INSTRUCTION_DECODER_INTERFACE::IMM_OP &&
-	   instruction_decoder->get_func3() == INSTRUCTION_DECODER_INTERFACE::ORI_FN3) {
-		auto rs1 = instruction_decoder->get_rs1();
-		auto rd = instruction_decoder->get_rd();
 
-		auto value = register_file->get_value_integer(rs1) | instruction_decoder->get_imm(31, 20);
-		register_file->set_value_integer(rd, value);
-	}
-
-	if(instruction_decoder->get_opcode() == INSTRUCTION_DECODER_INTERFACE::IMM_OP &&
-	   instruction_decoder->get_func3() == INSTRUCTION_DECODER_INTERFACE::XORI_FN3) {
-		auto rs1 = instruction_decoder->get_rs1();
-		auto rd = instruction_decoder->get_rd();
-
-		auto value = register_file->get_value_integer(rs1) ^ instruction_decoder->get_imm(31, 20);
-		register_file->set_value_integer(rd, value);
-	}
 }
