54d82a0e814fcafa2d1fc6f2efb53700afb7e71f
hsufit <a167349852@hotmail.com>
2021-09-25 23:45:18 +0800

    Extract execution behavior to *_E functions

--- executor.cpp
+++ executor.cpp
@@ -9,49 +9,16 @@ void EXECUTOR::execute()
 		case INSTRUCTION_DECODER_INTERFACE::IMM_OP:
 			switch (instruction_decoder->get_func3()) {
 				case INSTRUCTION_DECODER_INTERFACE::ADDI_FN3:
-					if(instruction_decoder->get_opcode() == INSTRUCTION_DECODER_INTERFACE::IMM_OP &&
-					   instruction_decoder->get_func3() == INSTRUCTION_DECODER_INTERFACE::ADDI_FN3) {
-						auto rs1 = instruction_decoder->get_rs1();
-						auto rd = instruction_decoder->get_rd();
-
-						auto value = register_file->get_value_integer(rs1) + instruction_decoder->get_imm(31, 20);
-						register_file->set_value_integer(rd, value);
-						//TODO: just for test, move these to decoder and logger in future
-						std::cout << "ADDI" << std::endl;
-						std::cout << "rs1: " << rs1 << std::endl;
-						std::cout << "rd: " << rd << std::endl;
-						std::cout << "value: " << value << std::endl;
-					}
+					ADDI_E();
 					break;
 				case INSTRUCTION_DECODER_INTERFACE::ANDI_FN3:
-					if(instruction_decoder->get_opcode() == INSTRUCTION_DECODER_INTERFACE::IMM_OP &&
-					   instruction_decoder->get_func3() == INSTRUCTION_DECODER_INTERFACE::ANDI_FN3) {
-						auto rs1 = instruction_decoder->get_rs1();
-						auto rd = instruction_decoder->get_rd();
-
-						auto value = register_file->get_value_integer(rs1) & instruction_decoder->get_imm(31, 20);
-						register_file->set_value_integer(rd, value);
-					}
+					ANDI_E();
 					break;
 				case INSTRUCTION_DECODER_INTERFACE::XORI_FN3:
-					if(instruction_decoder->get_opcode() == INSTRUCTION_DECODER_INTERFACE::IMM_OP &&
-					   instruction_decoder->get_func3() == INSTRUCTION_DECODER_INTERFACE::ORI_FN3) {
-						auto rs1 = instruction_decoder->get_rs1();
-						auto rd = instruction_decoder->get_rd();
-
-						auto value = register_file->get_value_integer(rs1) | instruction_decoder->get_imm(31, 20);
-						register_file->set_value_integer(rd, value);
-					}
+					ORI_E();
 					break;
 				case INSTRUCTION_DECODER_INTERFACE::ORI_FN3:
-					if(instruction_decoder->get_opcode() == INSTRUCTION_DECODER_INTERFACE::IMM_OP &&
-					   instruction_decoder->get_func3() == INSTRUCTION_DECODER_INTERFACE::XORI_FN3) {
-						auto rs1 = instruction_decoder->get_rs1();
-						auto rd = instruction_decoder->get_rd();
-
-						auto value = register_file->get_value_integer(rs1) ^ instruction_decoder->get_imm(31, 20);
-						register_file->set_value_integer(rd, value);
-					}
+					XORI_E();
 					break;
 				default:
 					std::cout << "INVALID: Func3 in IMM_OP :" << instruction_decoder->get_func3() << std::endl;
@@ -62,7 +29,45 @@ void EXECUTOR::execute()
 			std::cout << "INVALID: Func3 in IMM_OP :" << instruction_decoder->get_func3() << std::endl;
 			break;
 	}
+}
 
+void EXECUTOR::ADDI_E()
+{
+						auto rs1 = instruction_decoder->get_rs1();
+						auto rd = instruction_decoder->get_rd();
 
+						auto value = register_file->get_value_integer(rs1) + instruction_decoder->get_imm(31, 20);
+						register_file->set_value_integer(rd, value);
+						//TODO: just for test, move these to decoder and logger in future
+						std::cout << "ADDI" << std::endl;
+						std::cout << "rs1: " << rs1 << std::endl;
+						std::cout << "rd: " << rd << std::endl;
+						std::cout << "value: " << value << std::endl;
+}
 
+void EXECUTOR::ANDI_E()
+{
+						auto rs1 = instruction_decoder->get_rs1();
+						auto rd = instruction_decoder->get_rd();
+
+						auto value = register_file->get_value_integer(rs1) & instruction_decoder->get_imm(31, 20);
+						register_file->set_value_integer(rd, value);
+}
+
+void EXECUTOR::ORI_E()
+{
+						auto rs1 = instruction_decoder->get_rs1();
+						auto rd = instruction_decoder->get_rd();
+
+						auto value = register_file->get_value_integer(rs1) | instruction_decoder->get_imm(31, 20);
+						register_file->set_value_integer(rd, value);
+}
+
+void EXECUTOR::XORI_E()
+{
+						auto rs1 = instruction_decoder->get_rs1();
+						auto rd = instruction_decoder->get_rd();
+
+						auto value = register_file->get_value_integer(rs1) ^ instruction_decoder->get_imm(31, 20);
+						register_file->set_value_integer(rd, value);
 }
--- executor.h
+++ executor.h
@@ -6,5 +6,10 @@ class EXECUTOR: public EXECUTOR_INTERFACE
 {
 public:
 	virtual void execute() override;
+private:
+	void ADDI_E();
+	void ANDI_E();
+	void ORI_E();
+	void XORI_E();
 };
 #endif //INC_EXECUTOR_H_
