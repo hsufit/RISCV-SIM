dacd8decfcf73c1cf17a16c0ea21fa902f053010
hsufit <a167349852@hotmail.com>
2021-09-27 23:16:39 +0800

    Add test binary code for SLLI, SRLI, and SRAI

--- cpu.cpp
+++ cpu.cpp
@@ -8,7 +8,7 @@ CPU::CPU(sc_module_name name) : sc_module(name)
 
 void CPU::hello_thread(void)
 {
-	for(int i = 0; i < 5; i++) {
+	for(int i = 0; i < 8; i++) {
 		step();
 		wait(delay);
 	}
@@ -19,12 +19,15 @@ void CPU::step()
 //       char data = address_space->read(pc, 1);
 //       std::cout << "time " << sc_core::sc_time_stamp() << ":" << "hello received!!" << data << std::endl;
 //       TODO:fake instructions, move to memory
-	auto fakeInstructionMemory = std::array<uint32_t, 5> {
-		0b000000000001'00000'000'00001'0010011,
-		0b111111111111'00001'000'00010'0010011,
-		0b000000000000'00000'000'00000'0010011,
-		0b100000000000'00000'010'00000'0010011,
-		0b100000000000'00000'011'00000'0010011,
+	auto fakeInstructionMemory = std::array<uint32_t, 8> {
+		0b000000000001'00000'000'00001'0010011, //ADDI x1 = x0 + 1
+		0b111111111111'00001'000'00010'0010011, //ADDI x2 = x1 + (-1)
+		0b000000000000'00000'000'00000'0010011, //NOP
+		0b100000000000'00001'010'00000'0010011, //SLTI x1 = x0 < negativeNum
+		0b100000000000'00010'011'00010'0010011, //SLTU x2 = x0 < PositiveNum
+		0b0000000'11111'00010'001'00011'0010011, //SLLI x3 = x2 << 31
+		0b0000000'00001'00011'101'00100'0010011, //SRLI x4 = x2 >> 1
+		0b0100000'00001'00011'101'00101'0010011, //SRAI x5 = x2 >> 1
 	};
 
 	instruction_decoder->set_instruction(fakeInstructionMemory[pc]);
