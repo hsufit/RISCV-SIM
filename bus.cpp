18851d197c2a6c8f1305212f48c8a8aa9ab96eab
hsufit <a167349852@hotmail.com>
2021-09-30 01:19:25 +0800

    Initialize data before used by b_transport

--- bus.cpp
+++ bus.cpp
@@ -1,6 +1,5 @@
 #include "bus.h"
 
-
 BUS::BUS(sc_module_name name): sc_module(name), memory("memory")
 {
 	this->memory_socket.bind(memory.memory_socket);
@@ -8,7 +7,7 @@ BUS::BUS(sc_module_name name): sc_module(name), memory("memory")
 
 int32_t BUS::read(uint32_t addr, uint32_t size)
 {
-	char data;
+	uint32_t data = 0;
 	tlm::tlm_generic_payload trans;
 	trans.set_command(tlm::TLM_READ_COMMAND);
 	trans.set_data_ptr(reinterpret_cast<unsigned char*>(&data));
@@ -16,6 +15,7 @@ int32_t BUS::read(uint32_t addr, uint32_t size)
 	trans.set_response_status(tlm::TLM_INCOMPLETE_RESPONSE);
 	trans.set_address(addr);
 	sc_core::sc_time delay = sc_core::SC_ZERO_TIME;
+
 	memory_socket->b_transport(trans, delay);
 	return data;
 }
