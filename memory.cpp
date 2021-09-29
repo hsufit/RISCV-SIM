81320df6f11a05e9b945c721fa3c2e8d63fdfc12
hsufit <a167349852@hotmail.com>
2021-09-30 01:18:28 +0800

    Move test binary code to memory

--- memory.cpp
+++ memory.cpp
@@ -2,7 +2,34 @@
 
 MEMORY::MEMORY(sc_module_name name) : sc_module(name)
 {
+	auto binary = std::array<uint32_t, 4096> {
+		0b000000000001'00000'000'00001'0010011, //ADDI x1 = x0 + 1
+		0b111111111111'00001'000'00010'0010011, //ADDI x2 = x1 + (-1)
+		0b000000000000'00000'000'00000'0010011, //NOP
+		0b100000000000'00001'010'00000'0010011, //SLTI x1 = x0 < negativeNum
+		0b100000000000'00010'011'00010'0010011, //SLTU x2 = x0 < PositiveNum
+		0b0000000'11111'00010'001'00011'0010011, //SLLI x3 = x2 << 31
+		0b0000000'00001'00011'101'00100'0010011, //SRLI x4 = x2 >> 1
+		0b0100000'00001'00011'101'00101'0010011, //SRAI x5 = x2 >> 1
+		0b00000000000000000001'00110'0110111, //LUI x6 = 1 << 12
+		0b00000000000000000001'00110'0010111, //AUIPC x6 = pc + (1 << 12)
+		0b0000001'00000'00001'00110'0000011, //LH x6 = sign_ext(read(1, 2))
+		0b0000001'00000'00101'00110'0000011, //LHU x6 = read(1, 2)
+	};
+	programLoader(binary);
 	memory_socket.register_b_transport(this, &MEMORY::b_transport);
+}
+void MEMORY::programLoader(std::array<uint32_t, 4096> &binary)
+{
+	for(auto instruction: binary) {
+		dataMemory.push_back(instruction & 0xFF);
+		dataMemory.push_back(instruction >> 8 & 0xFF);
+		dataMemory.push_back(instruction >> 16 & 0xFF);
+		dataMemory.push_back(instruction >> 24 & 0xFF);
+
+		if(instruction != 0)
+			std::cout << "inst: " << instruction << std::endl;
+	}
 }
 
 void MEMORY::b_transport(tlm::tlm_generic_payload &trans, sc_core::sc_time &delay)
--- memory.h
+++ memory.h
@@ -16,11 +16,8 @@ public:
 private:
 	void b_transport(tlm::tlm_generic_payload &trans, sc_core::sc_time &delay);
 
-	std::vector<char> dataMemory{'M', 'o', 'o', 'n', '!',
-		',', ' ', '3', '.', '1',
-		'4', '1', '5', '9', '2',
-		'6', '5', '3', '5', '8',
-		'9', '7', '9', '3', '2',
-		'3', '8', '4', '6', '2'};
+
+	std::vector<uint8_t> dataMemory;
+	void programLoader(std::array<uint32_t, 4096> &binary);
 };
 #endif //INC_MEMORY_H_
