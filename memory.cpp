9e33476aecac83dc23dacb6f4761e9c257b76f7c
hsufit <a167349852@hotmail.com>
2021-09-30 01:08:11 +0800

    Implement multiple byte read/write request

--- memory.cpp
+++ memory.cpp
@@ -12,12 +12,22 @@ void MEMORY::b_transport(tlm::tlm_generic_payload &trans, sc_core::sc_time &dela
 	sc_dt::uint64 adr = trans.get_address();
 	unsigned char *ptr = trans.get_data_ptr();
 	unsigned int len = trans.get_data_length();
-	if(adr < dataMemory.size()) {
-		*(char*)ptr = dataMemory[adr];
-		std::cout << "time " << sc_core::sc_time_stamp() << ":" << "moon response!!" << dataMemory[adr] << std::endl;
-		trans.set_response_status(tlm::TLM_OK_RESPONSE);
-	} else {
-		trans.set_response_status(tlm::TLM_BURST_ERROR_RESPONSE);
-		std::cout << "error an address " << adr << "!!" << std::endl;
+
+	trans.set_response_status(tlm::TLM_OK_RESPONSE);
+
+	for(int i =  0; i < len; i++) {
+		if(adr < dataMemory.size()) {
+			if(cmd == tlm::TLM_READ_COMMAND) {
+				((char *)ptr)[i] = dataMemory[adr + i];
+			} else if(cmd == tlm::TLM_WRITE_COMMAND) {
+				dataMemory[adr + i] = ((char *)ptr)[i];
+			} else {
+				std::cout << "unsupported operation" << adr << "!!" << std::endl;
+			}
+		} else {
+			trans.set_response_status(tlm::TLM_BURST_ERROR_RESPONSE);
+			std::cout << "error an address " << adr << "!!" << std::endl;
+			return;
+		}
 	}
 }
